6b759c57ec54f74623ebbaa317a825b73bab28bc
VengerAlex <alexvenger90@gmail.com>
2021-08-18 16:12:27 +1200

    add style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import styled from 'styled-components';
+import GlobalStyle from "./components/GlobalStyle";
+
 import AboutUsPage from "./pages/AboutUsPage";
 
 const App = (props) => {
     return (
         <div className='App'>
+            <GlobalStyle />
             <AboutUsPage/>
         </div>
     )
 }
+
 
 export default App;
