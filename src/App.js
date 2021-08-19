7abbb8886c4768194b9e3295b80a37803de04a66
VengerAlex <alexvenger90@gmail.com>
2021-08-19 12:42:34 +1200

    end

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import GlobalStyle from "./components/GlobalStyle";
-import {Route, Switch} from "react-router-dom";
+import {Route, Switch, useLocation} from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
 
 import AboutUsPage from "./pages/AboutUsPage";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
 import Nav from "./components/Nav";
-import MovieDetails from "./components/MovieDetails";
+import MovieFeat from "./components/MovieDetails";
 
 const App = () => {
+    const location = useLocation()
+
     return (
         <div className='App'>
             <Nav/>
             <GlobalStyle/>
 
-            <Switch>
-                <Route exact path='/'>
-                    <AboutUsPage/>
-                </Route>
-                <Route exact path='/work'>
-                    <OurWork/>
-                </Route>
-                <Route path='/work/:id'>
-                    <MovieDetails />
-                </Route>
-                <Route path='/contact'>
-                    <ContactUs/>
-                </Route>
-            </Switch>
+            <AnimatePresence exitBeforeEnter>
+                <Switch location={location} key={location.pathname}>
+                    <Route exact path='/'>
+                        <AboutUsPage/>
+                    </Route>
+                    <Route exact path='/work'>
+                        <OurWork/>
+                    </Route>
+                    <Route path="/work/:id">
+                        <MovieFeat/>
+                    </Route>
+                    <Route path='/contact'>
+                        <ContactUs/>
+                    </Route>
+                </Switch>
+            </AnimatePresence>
         </div>
     )
 }
